9433322acb543811da0c6902b6a83f142a67b7d3
mavifwijaya <m.avifwijaya@gmail.com>
2017-05-19 22:43:44 +0700

    Update JavaApp4.java

--- src/topik3/JavaApp4.java
+++ src/topik3/JavaApp4.java
@@ -1,5 +1,49 @@
 package topik3;
 
     public class JavaApp4 {
-    
+    public int jum;
+    public int jum2;
+    public int jum3;
+    public void clan1(){
+        int a=45;
+        System.out.println("Barbarian   "+"Atk Power "+a);
+        int b=50;
+        System.out.println("Archer      "+"Atk Power "+b);
+        int c=70;
+        System.out.println("Dragon      "+"Atk Power "+c);
+        int d=65;
+        System.out.println("Hog         "+"Atk Power "+d);
+        int e=55;
+        System.out.println("Ballon      "+"Atk Power "+e);
+        jum = a+b+c+d+e;
+        System.out.println("Jumlah Atk Power    "+jum);
+    }
+    public void clan2(){
+        int a=68;
+        System.out.println("Arthas      "+"Atk Power "+a);
+        int b=55;
+        System.out.println("Lich King   "+"Atk Power "+b);
+        int c=70;
+        System.out.println("Muradin     "+"Atk Power "+c);
+        int d=85;
+        System.out.println("Illidan     "+"Atk Power "+d);
+        int e=75;
+        System.out.println("Tyrande     "+"Atk Power "+e);
+        jum2 = a+b+c+d+e;
+        System.out.println("Jumlah Atk Power    "+jum2);
+    }
+    public void clan3(){
+        int a=70;
+        System.out.println("Arthas      "+"Atk Power "+a);
+        int b=57;
+        System.out.println("Lich King   "+"Atk Power "+b);
+        int c=72;
+        System.out.println("Muradin     "+"Atk Power "+c);
+        int d=84;
+        System.out.println("Illidan     "+"Atk Power "+d);
+        int e=77;
+        System.out.println("Tyrande     "+"Atk Power "+e);
+        jum3 = a+b+c+d+e;
+        System.out.println("Jumlah Atk Power    "+jum3);
+    }
 }
