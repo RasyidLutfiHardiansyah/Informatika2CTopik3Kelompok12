58d89311ce3031598f7c9d10438d1bd3391bba3a
Rasyid Lutfi Hardiansyah <rasyidaam@gmail.com>
2017-05-19 22:50:24 +0700

    Update JavaApp.java

--- src/topik3/JavaApp.java
+++ src/topik3/JavaApp.java
@@ -17,6 +17,35 @@ public class JavaApp {
         System.out.println("2.Melihat Info Clan Dari Yang Terupdate");
         System.out.println("3.Info Clan Dengan Trophy Terbanyak");
         System.out.println("Pilih Menu ");
-       
+        Scanner scan = new Scanner(System.in);
+        x = scan.nextInt();
+        if(x == 1){
+            jv2.clan1();
+            jv2.clan2();
+            jv2.clan3();
+        }else if(x == 2){
+            s1.push("Dangerous");
+            s1.push("Imba");
+            s1.push("Rescue");
+            System.out.println(s1.pop());
+            System.out.println(s1.pop());
+            System.out.println(s1.pop());
+        }else if(x == 3){
+            System.out.println("Clan dengan trophy terbesar");
+            for(int i = 0;i <cslul.length;i++){
+                int index = i;
+                for(int j = i+1;j<cslul.length;j++)
+                    if(cslul[j]<cslul[index])
+                        index = j;
+                int smll = cslul[index];
+                cslul[index] = cslul[i];
+                cslul[i] = smll;
+            }
+            for(int i:cslul){
+                System.out.println(i);
+            }
+        }
+        }while(true);
     }
+         
 }
