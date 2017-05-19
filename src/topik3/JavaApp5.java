2951dc052f214bb304be78a6c2cdaa5bee2069b6
erlanggamartyo <martyoerlangga4@gmail.com>
2017-05-19 22:45:25 +0700

    Update JavaApp5.java

--- src/topik3/JavaApp5.java
+++ src/topik3/JavaApp5.java
@@ -1,5 +1,15 @@
 package topik3;
 
     public class JavaApp5 {
+        public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String id;
+    public JavaApp5(String id){
+        this.id = id;
+    }
    }
