98c403baef58469705b4c0d565324432603144ef
erlanggamartyo <martyoerlangga4@gmail.com>
2017-05-19 22:47:38 +0700

    Update JavaApp.java

--- src/topik3/JavaApp.java
+++ src/topik3/JavaApp.java
@@ -1,8 +1,22 @@
 package topik3;
-
+import java.util.Scanner;
+import java.util.Stack;
 public class JavaApp {
      
     public static void main(String[] args) {
+         JavaApp2 jv2 = new JavaApp2();
+        JavaApp4 jv4 = new JavaApp4();
+        int[]cslul = {353,285};                
+//285
+//353
+        Stack<String>s1 = new Stack<String>();
+        int x;
+        do{
+        System.out.println("-----------------------------------");
+        System.out.println("1.Info Clan");
+        System.out.println("2.Melihat Info Clan Dari Yang Terupdate");
+        System.out.println("3.Info Clan Dengan Trophy Terbanyak");
+        System.out.println("Pilih Menu ");
        
     }
 }
